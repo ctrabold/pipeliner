50e61075d0996f9e1502762b5f89840584623f33
Christian Trabold <ctrabold@thoughtworks.com>
2016-03-17 22:57:44 +0800

    Pipeliner: scaffolding a HTML model
    Just to get a feel how we can realize the view with pure HTML

--- Pipeliner.elm
+++ Pipeliner.elm
@@ -1,6 +1,27 @@
 module Pipeliner where
 
-import Html
+import Html exposing (..)
 
 main =
-  Html.text "Hello, Pipelines!"
+  div []
+  [
+    h1 [] [
+      text "Hello, Pipelines!"
+    ],
+    hr [] [],
+    div []
+    [
+      h2 [] [
+        text "Build #3"
+      ],
+      hr [] [],
+      h2 [] [
+        text "Build #2"
+      ],
+      hr [] [],
+      h2 [] [
+        text "Build #1"
+      ],
+      hr [] []
+    ]
+  ]
