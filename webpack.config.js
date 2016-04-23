cd7622df1cf439e6c86aa27571c9058cb4443425
Christian Trabold <ctrabold@thoughtworks.com>
2016-04-23 17:11:34 +0800

    webpack: simplify config

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,14 @@
 module.exports = {
   entry: "./main.js",
   output: {
-    path: __dirname + '/assets/javascripts',
+    path: "./assets/javascripts",
     filename: "bundle.js"
   },
   module: {
     loaders: [{
       test: /\.elm$/,
-      exclude: [/elm-stuff/, /node_modules/],
-      loader: 'elm-webpack'
+      loader: "elm-webpack"
     }],
     noParse: [/.elm$/]
   }
 };
-
